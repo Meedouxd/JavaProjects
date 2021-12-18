a961bef340424da30e5d216df0263237b937a612
Meedouxd <54485614+Meedouxd@users.noreply.github.com>
2021-12-18 10:32:01 -0500

    Update Main.java

--- MinecraftNameSearcher/src/Main.java
+++ MinecraftNameSearcher/src/Main.java
@@ -1,5 +1,3 @@
-//C:\Users\jmorg\Desktop\names.txt
-//C:\Users\jmorg\Desktop\mcnames\namesearchpool\summore.txt
 public class Main {
     public static void main(String args[]){
         GUI gui = new GUI();
